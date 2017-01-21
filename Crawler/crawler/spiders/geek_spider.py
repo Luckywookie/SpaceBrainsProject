cdbb5a96d82b9e14e184fb4e0e67680ea8570d1e
van4oza <chezhik@gmail.com>
2017-01-21 07:49:57 +0300

    Подключил urllib.parse

--- Crawler/crawler/spiders/geek_spider.py
+++ Crawler/crawler/spiders/geek_spider.py
@@ -6,6 +6,8 @@ from crawler.items import BrainItemLoader, BrainItem
 
 import pymysql
 
+from urllib.parse import *
+
 
 class GeekSitemapSpider(SitemapSpider):
     name = 'geek_sitemap_spider'
@@ -19,6 +21,8 @@ class GeekSitemapSpider(SitemapSpider):
     for row in cursor:
         print(row['Name'])
         sites.append('http://' + row['Name'] + '/robots.txt')
+        s = urlparse('//' + row['Name'], scheme='http')
+        print(s.geturl())
 
     cursor.close()
     cnx.close()
@@ -26,44 +30,45 @@ class GeekSitemapSpider(SitemapSpider):
     sitemap_urls = sites
 
     def parse(self, response):
-        cnx = pymysql.connect(host='localhost', user='root', password='', db='ratepersons', charset='utf8mb4',
-                              cursorclass=pymysql.cursors.DictCursor)
-        cursor = cnx.cursor()
-        sql = 'INSERT INTO Pages (Url) VALUES ({})'.format(response.url)
-        cursor.execute(sql)
-        cnx.commit()
-        cnx.close()
+        # cnx = pymysql.connect(host='localhost', user='root', password='', db='ratepersons', charset='utf8mb4',
+        #                       cursorclass=pymysql.cursors.DictCursor)
+        # cursor = cnx.cursor()
+        # sql = 'INSERT INTO Pages (Url) VALUES ({})'.format(response.url)
+        # cursor.execute(sql)
+        # cnx.commit()
+        # cnx.close()
+        pass
 
 
-class GeekSpider(CrawlSpider):
-    name = 'geek_spider'
-
-    start_urls = ['https://geekbrains.ru/courses']
-    allowed_domains = ['geekbrains.ru']
-
-    rules = (
-        Rule(
-            LinkExtractor(
-                restrict_xpaths=['//div[@class="searchable-container"]'],
-                allow=r'https://geekbrains.ru/\w+/\d+$'
-            ),
-            callback='parse_item'
-        ),
-    )
-
-    def parse_item(self, response):
-        selector = Selector(response)
-        l = BrainItemLoader(BrainItem(), selector)
-        l.add_value('url', response.url)
-        l.add_xpath('title', '//div[@class="div padder-v m-r m-l"]'
-                             '/h1/text()')
-        l.add_xpath('subtitle', '//div[@class="div padder-v m-r m-l"]'
-                                '/div[@class="h2"]/text()')
-        l.add_xpath('logo', '//div[@class="text-center m-b-lg"]'
-                            '/img/@src')
-        l.add_xpath('description', '//div[@class="m-b-xs m-t m-r-xl"]'
-                                   '/p/text()')
-        l.add_xpath('teachers', '//div[@id="authors"]'
-                                '//p[@class="m-l m-b-xs m-t-xs m-r-xs"]'
-                                '/text()')
-        return l.load_item()
+# class GeekSpider(CrawlSpider):
+#     name = 'geek_spider'
+#
+#     start_urls = ['https://geekbrains.ru/courses']
+#     allowed_domains = ['geekbrains.ru']
+#
+#     rules = (
+#         Rule(
+#             LinkExtractor(
+#                 restrict_xpaths=['//div[@class="searchable-container"]'],
+#                 allow=r'https://geekbrains.ru/\w+/\d+$'
+#             ),
+#             callback='parse_item'
+#         ),
+#     )
+#
+#     def parse_item(self, response):
+#         selector = Selector(response)
+#         l = BrainItemLoader(BrainItem(), selector)
+#         l.add_value('url', response.url)
+#         l.add_xpath('title', '//div[@class="div padder-v m-r m-l"]'
+#                              '/h1/text()')
+#         l.add_xpath('subtitle', '//div[@class="div padder-v m-r m-l"]'
+#                                 '/div[@class="h2"]/text()')
+#         l.add_xpath('logo', '//div[@class="text-center m-b-lg"]'
+#                             '/img/@src')
+#         l.add_xpath('description', '//div[@class="m-b-xs m-t m-r-xl"]'
+#                                    '/p/text()')
+#         l.add_xpath('teachers', '//div[@id="authors"]'
+#                                 '//p[@class="m-l m-b-xs m-t-xs m-r-xs"]'
+#                                 '/text()')
+#         return l.load_item()
