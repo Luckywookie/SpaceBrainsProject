95e9e858ab19a7358d3f75bb84e22803c246daeb
van4oza <chezhik@gmail.com>
2017-01-26 08:45:09 +0300

    Добавил сэт из пройденных сайтов
    explored_sites = set()

--- Crawler/crawler/spiders/geek_spider.py
+++ Crawler/crawler/spiders/geek_spider.py
@@ -1,10 +1,10 @@
-from scrapy.spiders import SitemapSpider
-
 # from scrapy.spiders import CrawlSpider, Rule, SitemapSpider
 # from scrapy.linkextractors import LinkExtractor
 # from scrapy.selector import Selector
 #
 # from crawler.items import BrainItemLoader, BrainItem
+
+from scrapy.spiders import SitemapSpider
 
 import pymysql
 
@@ -17,18 +17,34 @@ cursor = db.cursor()
 
 sitemaps = []
 site_ids = {}
+explored_sites = set()
 
 cursor.execute('SELECT * FROM Sites')
 
 for row in cursor:
     print('Site: {}\nSite ID: {}'.format(row['Name'], row['ID']))
-    sitemap_url = urlunparse(('http', row['Name'], '/robots.txt', '', '', ''))
+    sitemap_url = urlunparse(('https', row['Name'], '/sitemap.xml', '', '', ''))
     site_ids[row['Name']] = row['ID']
     sitemaps.append(sitemap_url)
     print('Sitemap URL: ' + sitemap_url)
 
 print(site_ids)
 print(sitemaps)
+
+cursor.execute('SELECT * FROM Pages')
+
+for row in cursor:
+    explored_sites.add(row['SiteID'])
+
+print(explored_sites)
+
+for site in site_ids:
+    if site_ids[site] not in explored_sites:
+        robot = urlunparse(('https', site, '/robots.txt', '', '', ''))
+        sql = 'INSERT INTO Pages (Url, SiteID, LastScanDate) VALUES (%s, %s, null)'
+        cursor.execute(sql, (robot, site_ids[site]))
+        explored_sites.add(site_ids[site])
+        db.commit()
 
 cursor.close()
 db.close()
@@ -40,7 +56,7 @@ class GeekSitemapSpider(SitemapSpider):
     sitemap_urls = sitemaps
 
     def parse(self, response):
-        url = urlparse(response.url)
+        url = urlparse(response.url, scheme='https')
         site_id = site_ids[url.netloc]
         print(response.url)
         print(site_ids[url.netloc])
@@ -49,7 +65,7 @@ class GeekSitemapSpider(SitemapSpider):
                              cursorclass=pymysql.cursors.DictCursor)
         cursor = db.cursor()
         sql = 'INSERT INTO Pages (Url, SiteID) VALUES (%s, %s)'
-        cursor.execute(sql, (response.url, site_id))
+        cursor.execute(sql, (url.path, site_id))
         db.commit()
         db.close()
 
