7435fda4cfa050f5a5da670aaa3b3a3b872578c8
Konstantin Savin <kostigan.savin@gmail.com>
2017-02-05 01:00:29 +0300

    Добавил взаимодействие с БД через репозиторий

--- Crawler_c/crawler_download.py
+++ Crawler_c/crawler_download.py
@@ -38,37 +38,44 @@ def get_page(url):
         response.raise_for_status()
 
 
-def findsitestorank(cursor):
+def findsitestorank():
     '''
     :param cursor: Курсор для взаимодействия с БД
     :return: Результат запроса к БД -> Список сайтов для обхода.
     '''
-    sql = 'select * from sites where id not in (select distinct siteid from pages)'
-    cursor.execute(sql)
-    result = cursor.fetchall()
+    # sql = 'select * from sites where id not in (select distinct siteid from pages)'
+    # cursor.execute(sql)
+    sitesrepository = repository.DbSiteReposytory()
+    siteworker = repository.SiteRepositoryWorker(sitesrepository)
+    result = siteworker.getsitestorank()
     return result
 
-
-def writerobotstodb(cursor, sites):
+def writerobotstodb(sites):
     '''
     :param cursor: Курсор для взаимодействия с БД
     :param sites: Список сайтов для обхода краулера
     Записываем для каждого сайта в pages ссылку на robots.txt
     '''
-    sql = "insert into `Pages` (Url, SiteID, FoundDateTime) values (%s, %s, %s) "
+    # sql = "insert into `Pages` (Url, SiteID, FoundDateTime) values (%s, %s, %s) "
+    pagesrepository = repository.DbPageRepository()
+    pagewoker = repository.PagesRepositoryWorker(pagesrepository)
     for site in sites:
-        url = urllib.parse.urlunsplit(('https', site['Name'], 'robots.txt', '', ''))
-        cursor.execute(sql, (url, site['ID'], datetime.datetime.today()))
+        url = urllib.parse.urlunparse(('https', site['Name'], 'robots.txt', '', '', ''))
+        page = repository.Page(Url=url, SiteID=site['ID'], FoundDateTime=datetime.datetime.today())
+        #cursor.execute(sql, (url, site['ID'], datetime.datetime.today()))
+        pagewoker.writepagestostore(page)
 
 
-def pagestowalk(cursor):
+def pagestowalk():
     '''
     :param cursor: Курсор для взаимодействия с БД
     :return: Список страниц для обхода у которых двта последнего обхода пустая
     '''
-    sql = "select * from `Pages` where `Pages`.`LastScanDate` is null"
-    cursor.execute(sql)
-    result = cursor.fetchall()
+    # sql = "select * from `Pages` where `Pages`.`LastScanDate` is null"
+    # cursor.execute(sql)
+    pagesrepository = repository.DbPageRepository()
+    pagewoker = repository.PagesRepositoryWorker(pagesrepository)
+    result = pagewoker.getpagelastscandatenull()
     return result
 
 
@@ -96,16 +103,34 @@ def readrobots(file):
             return x.split(':', maxsplit=1)[-1].strip()
 
 
-def writeurl(cursor, url, siteid):
+def writeurl(url, siteid):
     '''
     :param cursor: Курсор для взаимодействия с БД
     :param url: Ссылка для записи в БД
     :param siteid: ID сайта для которого записываем ссылку в БД
     :return:
     '''
-    sql = "insert into `Pages` (Url, SiteID, FoundDateTime) values (%s, %s, %s) "
+    pagesrepository = repository.DbPageRepository()
+    pagewoker = repository.PagesRepositoryWorker(pagesrepository)
+    # sql = "insert into `Pages` (Url, SiteID, FoundDateTime) values (%s, %s, %s) "
     print('Пишем url в БД')
-    cursor.execute(sql, (url, siteid, datetime.datetime.today()))
+    page = repository.Page(Url=url, SiteID=siteid, FoundDateTime=datetime.datetime.today())
+    # cursor.execute(sql, (url, site['ID'], datetime.datetime.today()))
+    pagewoker.writepagestostore(page)
+    # cursor.execute(sql, (url, siteid, datetime.datetime.today()))
+
+
+def updatelastscandate(pageid):
+    """
+    :param pageid:
+    :return:
+    """
+    pagesrepository = repository.DbPageRepository()
+    pagewoker = repository.PagesRepositoryWorker(pagesrepository)
+    page = repository.Page(ID=pageid, LastScanDate=datetime.datetime.today())
+    # cursor.execute(sql, (url, site['ID'], datetime.datetime.today()))
+    pagewoker.updatepageinstore(page)
+    # cursor.execute(sql, (url, siteid, datetime.datetime.today()))
 
 
 def sitemapparse(html):
@@ -143,15 +168,20 @@ def countstatforpage(cursor, html): #cursor = repository
     :param html: HTML страницы которую анализируем на предмет сколько раз встречается ключевые слова.
     :return: Словаь по персонам с ID персоны и статистика для проанализируемой странице
     '''
-    sql = "select * from `Persons`"
-    cursor.execute(sql)
-    personslist = cursor.fetchall()
+    personrepository = repository.DbPersonRepository()
+    personworker = repository.PersonRepositoryWorker(personrepository)
+    keywordrepository = repository.DbKeywordRepository()
+    keywordworker = repository.KeywordRepositoryWorker(keywordrepository)
+    # sql = "select * from `Persons`"
+    # cursor.execute(sql)
+    personslist = personworker.getpersons()
     personsdict = {}
     for person in personslist:
         lst = []
-        sql = "select * from `Keywords` where `Keywords`.`PersonID`=%s"
-        cursor.execute(sql, (person['ID'], )) #GetKeywordByPersonID
-        keywordslist = cursor.fetchall() #GetKeywordByPersonID
+
+        # sql = "select * from `Keywords` where `Keywords`.`PersonID`=%s"
+        # cursor.execute(sql, (person['ID'], )) #GetKeywordByPersonID
+        keywordslist = keywordworker.getbypersonid(person['ID']) #GetKeywordByPersonID
 
         for keyword in keywordslist:
             #lst.append((html.count(keyword['Name']), keyword['Name']))
@@ -163,6 +193,23 @@ def countstatforpage(cursor, html): #cursor = repository
     return personsdict
 
 
+def writerank(peronid, pageid, rank):
+    '''
+    :param cursor: Курсор для взаимодействия с БД
+    :param url: Ссылка для записи в БД
+    :param siteid: ID сайта для которого записываем ссылку в БД
+    :return:
+    '''
+    personpagerankrepository = repository.DbPersonPageRankRepository()
+    personpagerankwoker = repository.PersonPageRankRepositoryWorker(personpagerankrepository)
+
+    print('Пишем Rank в БД')
+    personpagerank = repository.PersonPageRank(PersonID=pageid, PageID=pageid, Rank=rank)
+    # cursor.execute(sql, (url, site['ID'], datetime.datetime.today()))
+    personpagerankwoker.writeranktostore(personpagerank)
+    # cursor.execute(sql, (url, siteid, datetime.datetime.today()))
+
+
 def main():
 
     cn = db_connect()
@@ -170,11 +217,14 @@ def main():
 
     while True:
         print('Находим сайты для обхода и записываем ссылку на robots.txt')
-        sites = findsitestorank(cur)
-        writerobotstodb(cur, sites)
-        cn.commit()
+        # sites = findsitestorank(cur)
+        sites = findsitestorank()
+        # writerobotstodb(cur, sites)
+        writerobotstodb(sites)
+        # cn.commit()
 
-        pages = pagestowalk(cur)
+        # pages = pagestowalk(cur)
+        pages = pagestowalk()
         print('Страниц для обхода ->', len(pages))
 
         if len(pages) > 0:
@@ -184,9 +234,10 @@ def main():
                     html = get_page(page['Url'])
                 except requests.exceptions.HTTPError:
                     print('HTTPError!!!')
-                    sql = 'update `Pages` set `LastScanDate`=%s where `Pages`.`ID` = %s'
-                    t = (datetime.date.today(), page['ID'])
-                    cur.execute(sql, t)
+                    # sql = 'update `Pages` set `LastScanDate`=%s where `Pages`.`ID` = %s'
+                    # t = (datetime.date.today(), page['ID'])
+                    # cur.execute(sql, t)
+                    updatelastscandate(page['ID'])
                     print(page)
                     continue
                 except requests.exceptions.ConnectionError as err:
@@ -197,28 +248,34 @@ def main():
                 if (whatisurl(page['Url'])) == 'robots':
                     print('Записываем ссылку на sitemap в БД')
                     sitemapurl = readrobots(html)
-                    writeurl(cur, sitemapurl, page['SiteID'])
-                    sql = 'update `Pages` set `LastScanDate`=%s where `Pages`.`ID` = %s'
-                    cur.execute(sql, (datetime.datetime.today(), page['ID']))
+                    # writeurl(cur, sitemapurl, page['SiteID'])
+                    writeurl(sitemapurl, page['SiteID'])
+                    # sql = 'update `Pages` set `LastScanDate`=%s where `Pages`.`ID` = %s'
+                    # cur.execute(sql, (datetime.datetime.today(), page['ID']))
+                    updatelastscandate(page['ID'])
                 elif (whatisurl(page['Url'])) == 'sitemap':
                     print('Получаем ссылки из sitemap и записываем в БД')
                     urlstowrite = sitemapparse(html)
                     for url in urlstowrite:
                         print(url)
-                        writeurl(cur, url, page['SiteID'])
-                        sql = 'update `Pages` set `LastScanDate`=%s where `Pages`.`ID` = %s'
-                        cur.execute(sql, (datetime.datetime.today(), page['ID']))
+                        # writeurl(cur, url, page['SiteID'])
+                        writeurl(url, page['SiteID'])
+                        #sql = 'update `Pages` set `LastScanDate`=%s where `Pages`.`ID` = %s'
+                        #cur.execute(sql, (datetime.datetime.today(), page['ID']))
+                        updatelastscandate(page['ID'])
                 else:                                           #Страница для анализа.
                     print(page['Url'])
                     d = countstatforpage(cur, html)
                     for pers, rank in d.items():
-                        #print(pers, rank)
-                        #print(page['ID'])
-                        sql = 'insert into `personpagerank` (personid, pageid, rank) values (%s, %s, %s)'
-                        cur.execute(sql, (pers, page['ID'], rank))
+                        # print(pers, rank)
+                        # print(page['ID'])
+                        # sql = 'insert into `personpagerank` (personid, pageid, rank) values (%s, %s, %s)'
+                        # cur.execute(sql, (pers, page['ID'], rank))
+                        writerank(pers, page['ID'], rank)
 
-                    sql = 'update `Pages` set `LastScanDate`=%s where `Pages`.`ID` = %s'
-                    cur.execute(sql, (datetime.datetime.today(), page['ID']))
+                    # sql = 'update `Pages` set `LastScanDate`=%s where `Pages`.`ID` = %s'
+                    # cur.execute(sql, (datetime.datetime.today(), page['ID']))
+                    updatelastscandate(page['ID'])
                 cn.commit()
                 i += 1                                                                          #Cделал для отладки
                 print('Осталось обойти : {} страниц из {}'.format(len(pages)-i, len(pages)))    #Cделал для отладки
