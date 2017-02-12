a304715d86aeb0a4e25e7a7a1bfe6421b98cffa4
BblDE3HAP <bblde3hap@users.noreply.github.com>
2017-02-12 10:26:30 +0700

    changed resources/keywords.Keyword.post() removed resources/keywords.CreateKeyword fix: remove spaces from password_gen() output

--- WebServerREST/resources/keyword.py
+++ WebServerREST/resources/keyword.py
@@ -37,15 +37,17 @@ class Keyword(Resource):
         return {'message': 'Item not found'}, 404
 
     @jwt_required()
-    def post(self, name):
+    def post(self):
+        data = Keyword.parser1.parse_args()
+        name = data['name']
+        person_id = ['person_id']
         if KeywordModel.find_by_name(name):
             return {
                 'message': "An keyword with name '{}' already exists.".format(
                     name)
             }, 400
 
-        data = Keyword.parser.parse_args()
-        keyword = KeywordModel(name=name, **data)
+        keyword = KeywordModel(name=name, person_id=person_id)
 
         try:
             keyword.save_to_db()
@@ -73,14 +75,15 @@ class Keyword(Resource):
 
         if keyword:
             keyword.name = data['name']
+            keyword.save_to_db()
+            return keyword.json(), 200
         else:
             keyword = KeywordModel(
                 name=data['name'],
                 person_id=data['person_id']
             )
-
-        keyword.save_to_db()
-        return keyword.json(), 200
+            keyword.save_to_db()
+            return keyword.json(), 201
 
 
 class KeywordList(Resource):
@@ -91,36 +94,3 @@ class KeywordList(Resource):
                 lambda x: x.json(), KeywordModel.query.all())
             )
         }, 200
-
-
-class CreateKeyword(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument(
-        'person_id',
-        type=int,
-        required=True,
-        help="Every keyword needs a person id."
-    )
-    parser.add_argument(
-        'name',
-        type=str,
-        required=True,
-        help="This field cannot be left blank!"
-    )
-
-    @jwt_required()
-    def post(self):
-        data = CreateKeyword.parser.parse_args()
-        keyword = KeywordModel(
-            name=data['name'],
-            person_id=data['person_id']
-        )
-
-        if KeywordModel.find_by_name(data['name']):
-            return {
-                'message': "A keyword with name '{}' already exists.".format(
-                    data['name'])
-            }, 400
-
-        keyword.save_to_db()
-        return keyword.json(), 201
